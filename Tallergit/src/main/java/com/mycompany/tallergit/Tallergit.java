08ca566793d0f24a015ac3f4b2ca16139a2ac735
Orion Touch <anthonygiraldo61@gmail.com>
2026-03-18 18:40:07 -0500

    test: pruebas nuevas

---
+++ Tallergit/src/main/java/com/mycompany/tallergit/Tallergit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tallergit;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Tallergit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
